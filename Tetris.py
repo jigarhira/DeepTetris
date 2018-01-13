d2771937c643b4336cafb7ec236fce2d65b5790a
Jigar Hira <jigarhira354@gmail.com>
2018-01-12 17:43:37 -0800

    Added graphics library Started framework of Tetris game

---
+++ Tetris.py
@@ -0,0 +1,100 @@
+import sys
+import pygame
+import numpy as np
+
+block_level =  0
+
+# All 7 different types of blocks
+block_styles = [[[0, 0, 0, 0],
+                 [0, 0, 0, 0],
+                 [1, 1, 1, 1],
+                 [0, 0, 0, 0]],
+
+                [[0, 0, 0, 0],
+                 [1, 0, 0, 0],
+                 [1, 1, 1, 0],
+                 [0, 0, 0, 0]],
+
+                [[0, 0, 0, 0],
+                 [0, 0, 1, 0],
+                 [1, 1, 1, 0],
+                 [0, 0, 0, 0]],
+
+                [[0, 0, 0, 0],
+                 [0, 1, 1, 0],
+                 [0, 1, 1, 0],
+                 [0, 0, 0, 0]],
+
+                [[0, 0, 0, 0],
+                 [0, 1, 1, 0],
+                 [1, 1, 0, 0],
+                 [0, 0, 0, 0]],
+
+                [[0, 0, 0, 0],
+                 [0, 1, 0, 0],
+                 [1, 1, 1, 0],
+                 [0, 0, 0, 0]],
+
+                [[0, 0, 0, 0],
+                 [1, 1, 0, 0],
+                 [0, 1, 1, 0],
+                 [0, 0, 0, 0]]]
+
+
+# Spawns a random block
+def block_spawn(board, block):
+    i = 0
+    for row in block:
+        j = 3
+        for element in row:
+            board[i][j] = element
+            j += 1
+        i += 1
+
+
+# Rotates a block
+def block_rotate(block):
+    for _ in range(3):
+        block = np.rot90(block)
+
+    return block
+
+
+# Makes the block fall 1 level
+def block_fall(board):
+    temp = np.zeros(10, dtype=int)
+
+    i = block_level
+    for i in range(i, i + 4):
+        for j in range(10):
+            temp[j] = board[i][j]
+            board[i][j] = board[i][j - 1]
+
+
+def run():
+    '''
+    pygame.init()
+
+    window_size = 200, 400
+
+    screen = pygame.display.set_mode(window_size)
+
+    open = True
+    while open:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                open = False
+
+    '''
+
+    board = np.zeros((22, 10), dtype=np.int)
+
+    block_spawn(board, block_rotate(block_styles[2]))
+
+    print(board)
+
+
+run()
